829a49a36da7e3870a7cdff03bb691d5a0cdb162
zattyuss <zattyuss@naver.com>
2022-05-25 13:45:10 +0900

    DTO생성

--- src/main/java/me/realtree/controller/HomeController.java
+++ src/main/java/me/realtree/controller/HomeController.java
@@ -1,5 +1,13 @@
 package me.realtree.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String Home() {
+		return "welcome";
+	}
 }
